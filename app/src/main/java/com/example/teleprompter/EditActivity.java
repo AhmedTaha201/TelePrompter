9bda88f8aac2382ca15d90010db8a0cb70903173
AhmedTaha201 <ahmedtaha201@gmail.com>
2018-07-21 18:00:05 +0200

    Use Toolbar as an action bar

--- app/src/main/java/com/example/teleprompter/EditActivity.java
+++ app/src/main/java/com/example/teleprompter/EditActivity.java
@@ -3,12 +3,22 @@ package com.example.teleprompter;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class EditActivity extends AppCompatActivity {
+
+    @BindView(R.id.edit_toolbar)
+    Toolbar toolbar;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
     }
 }
--- app/src/main/java/com/example/teleprompter/MainActivity.java
+++ app/src/main/java/com/example/teleprompter/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.teleprompter;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.main_toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
     }
 }
